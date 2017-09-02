a4f0774c34ec721432f8deedbb6408b1bcac364b
Rahul Kesavan <Rahul.Kesavan@watchguard.com>
2017-09-02 11:19:08 -0700

    feat: Reactored BookShelf.js and added fix to handle the case when book is not in any shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function BookShelf(props) {
+const BookShelf = (props) => {
     
-    function getText(currShelf, val, text) {
-        return val === currShelf ? `\u2714 ${text}` : `\u2002\u2002 ${text}`;     
-    }
+    const getText = (currShelf, val, text) => val === currShelf ? `\u2714 ${text}` : `\u2002\u2002 ${text}`;     
+    const handleChange = (e) => props.shelfUpdated(e.target.value, props.book)
     return ( 
-        <select value={ props.book.shelf }
-                onChange={ (e) => props.shelfUpdated(e.target.value, props.book) }>
+        <select value={ props.book.shelf || 'none'}
+                onChange={ handleChange }>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">{getText(props.book.shelf, 'currentlyReading', 'Currently Reading')}</option>
             <option value="wantToRead">{getText(props.book.shelf, 'wantToRead', 'Want To Read')}</option>
@@ -15,5 +15,10 @@ function BookShelf(props) {
             <option value="none">{getText(props.book.shelf, 'none', 'None')}</option>
         </select>
     );
+}
+
+BookShelf.prototypes = {
+    book: PropTypes.object.isRequired,
+    shelfUpdated: PropTypes.func.isRequired
 }
 export default BookShelf;
